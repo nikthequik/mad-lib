564bb9c63d164bd2c20e401e3e19ef9a3d05fd94
nikthequik <quik.the.nick@gmail.com>
2016-06-20 11:47:08 -0600

    implement controller and remove ng-init for setup section

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-angular.module('mad-lib',[])
-.controller('myCtrl', function())
+angular.module('madLib',[])
+.controller('myCtrl', function($scope){
+	$scope.male_name='male name',
+	$scope.dirty_task='dirty task',
+	$scope.obnoxious_celebrity='obnoxious celebrity',
+	$scope.job_title='job title',
+	$scope.celebrity='celebrity',
+	$scope.huge_number='huge number',
+	$scope.tedious_task='tedious task',
+	$scope.useless_skill='useless skill',
+	$scope.adjective='adjective',
+	$scope.gender='male'
+});
