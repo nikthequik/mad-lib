d88e0de28809411e1c77b1d4ee7f6a14a90ea4ae
nikthequik <quik.the.nick@gmail.com>
2016-06-22 18:43:02 -0600

    start adding form and validation

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
 angular.module('madLib',[])
 .controller('MyCtrl', function($scope){
 	var vm = this;
-	vm.male_name='male name';
-	vm.dirty_task='dirty task';
-	vm.obnoxious_celebrity='obnoxious celebrity';
-	vm.job_title='job title';
-	vm.celebrity='celebrity';
-	vm.huge_number='huge number';
-	vm.tedious_task='tedious task';
-	vm.useless_skill='useless skill';
-	vm.adjective='adjective';
-	vm.gender='male';
+	
 });
